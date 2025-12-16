fdfdfdaeb35f4f4806b3c1060e04ffb986248111
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2025-12-16 12:29:50 +0330

    add course , session , comment Model

---
+++ models/course.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    support: {
+      type: String,
+      required: true,
+    },
+    href: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+    categoryID: {
+      type: mongoose.Types.ObjectId,
+      ref: "category",
+      require: true,
+    },
+    creator: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+schema.virtual("sessions", {
+  ref: "Session",
+  localField: "_id",
+  foreignField: "course",
+});
+schema.virtual("comments", {
+  ref: "Comment",
+  localField: "_id",
+  foreignField: "course",
+});
+
+model = mongoose.model("Course", schema);
+module.exports = model;
