b478afd6a57eb9dcd05f9ae6bf526912913d7420
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2025-12-22 13:57:56 +0330

    add accept or reject comments feature by admin

--- controllers/v1/comment.js
+++ controllers/v1/comment.js
@@ -1,3 +1,4 @@
+const mongoose = require ('mongoose')
 const commentModel = require("./../../models/comment");
 const courseModel = require("./../../models/course");
 
@@ -18,3 +19,41 @@ exports.create = async (req, res) => {
   });
   return res.status(201).json(comment)
 };
+exports.remove = async (req , res)=>{
+  const {id}= req.params
+  const isValid = mongoose.Types.ObjectId.isValid(id)
+  if (!isValid){
+    return res.status(409).json({message : "comment ID is not valid"})
+  }
+  const deletedComment = await commentModel.findOneAndDelete({_id :id})
+  if (!deletedComment){
+   return res.status(404).json({message:"comment not found"})
+  }
+    return res.status(200).json(deletedComment)
+}
+exports.accept = async (req , res)=>{
+    const {id}= req.params
+  const isValid = mongoose.Types.ObjectId.isValid(id)
+  if (!isValid){
+    return res.status(409).json({message : "comment ID is not valid"})
+  }
+  const acceptedComment = await commentModel.findOneAndUpdate({_id : id}, {isAccept:1})
+   if (!acceptedComment){
+   return res.status(404).json({message:"comment not found"})
+  }
+  return res.status(200).json({message : " comment accepted"})
+
+}
+exports.reject = async (req , res)=>{
+    const {id}= req.params
+  const isValid = mongoose.Types.ObjectId.isValid(id)
+  if (!isValid){
+    return res.status(409).json({message : "comment ID is not valid"})
+  }
+  const rejectedComment = await commentModel.findOneAndUpdate({_id : id}, {isAccept:0})
+   if (!rejectedComment){
+   return res.status(404).json({message:"comment not found"})
+  }
+  return res.status(200).json({message : " comment rejected"})
+}
+ 
--- routes/v1/comment.js
+++ routes/v1/comment.js
@@ -3,10 +3,18 @@ const authMiddleware = require("./../../middlewares/auth");
 const isAdminMiddleware = require("./../../middlewares/isAdmin");
 const commentController = require("./../../controllers/v1/comment");
 
-const router = express.Router()
+const router = express.Router();
 
+router.route("/").post(authMiddleware, commentController.create);
+router
+  .route("/:id")
+  .delete(authMiddleware, isAdminMiddleware, commentController.remove);
 
-router.route("/").post(authMiddleware,commentController.create)
+router
+  .route("/:id/accept")
+  .put(authMiddleware, isAdminMiddleware, commentController.accept);
+router
+  .route("/:id/reject")
+  .put(authMiddleware, isAdminMiddleware, commentController.reject);
 
-
-module.exports = router
+module.exports = router;
