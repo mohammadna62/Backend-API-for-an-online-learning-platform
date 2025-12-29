33f1b140357b2a6887818e5f1a46f4ec9a4da025
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2025-12-29 11:06:05 +0330

    add remove discount option  by admin

--- controllers/v1/off.js
+++ controllers/v1/off.js
@@ -1,17 +1,26 @@
 const offModel = require("./../../models/Off");
 const courseModel = require("./../../models/course");
+const mongoose = require("mongoose");
 
 exports.getAll = async (req, res) => {
-    const offs = await offModel.find({},"-__v").populate("course", "name href").populate("creator","name")
-    return res.status(200).json(offs)
+  const offs = await offModel
+    .find({}, "-__v")
+    .populate("course", "name href")
+    .populate("creator", "name");
+  return res.status(200).json(offs);
 };
 
 exports.create = async (req, res) => {
   const { code, percent, course, max } = req.body;
   const newOff = await offModel.create({
-   code, percent, course, max , creator:req.user._id ,uses :0 
-  })
-  return res.status(201).json(newOff)
+    code,
+    percent,
+    course,
+    max,
+    creator: req.user._id,
+    uses: 0,
+  });
+  return res.status(201).json(newOff);
 };
 exports.setOnAll = async (req, res) => {
   const { discount } = req.body;
@@ -28,4 +37,12 @@ exports.setOnAll = async (req, res) => {
 
 exports.getOne = async (req, res) => {};
 
-exports.remove = async (req, res) => {};
+exports.remove = async (req, res) => {
+  const { id } = req.params;
+  const isValid = mongoose.Types.ObjectId.isValid(id);
+  if (!isValid) {
+    return res.status(209).json({ message: "discount ID is not valid" });
+  }
+  const deleteDiscount = await offModel.findOneAndDelete({ _id: id });
+  return res.status(200).json(deleteDiscount);
+};
