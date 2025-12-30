86611b0a08f71fe5f4aed866f7003a61c4a4a2d3
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2025-12-30 09:23:02 +0330

    modify funcs and add getOne

--- controllers/v1/article.js
+++ controllers/v1/article.js
@@ -1,7 +1,7 @@
 const articleModel = require("./../../models/article");
 
 exports.getAll = async (req, res) => {
-  const articles = await articleModel.find({ publish: 1 });
+  const articles = await articleModel.find({ publish: 1 }).populate("creator" ,"name").populate("categoryID");
   if (!articles) {
     return res
       .status(409)
@@ -25,14 +25,26 @@ exports.create = async (req, res) => {
   });
   return res.status(201).json(article);
 };
-exports.getOne = async (req, rs) => {};
+exports.getOne = async (req, res) => {
+  const {href}= req.params
+  const articles = await articleModel.find({href:req.params.href,publish:1}).lean()
+  
+  if(articles.length===0){
+    return res.status(404).json({message: "there is not any article with this subject"})
+  }else{
+    
+    return res.status(200).json(articles)
+  }
+};
 
-exports.remove = async (req, rs) => {};
+exports.remove = async (req, res) => {
 
-exports.saveDraft = async (req, rs) => {};
+};
 
-exports.getAllByAdmin = async (req, rs) => {
-  const articles = await articleModel.find({});
+exports.saveDraft = async (req, res) => {};
+
+exports.getAllByAdmin = async (req, res) => {
+  const articles = await articleModel.find({}).populate("creator" ,"name").populate("categoryID");
   if (!articles) {
     return res
       .status(409)
